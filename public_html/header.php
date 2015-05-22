83dab6161ee7478f05e6e343111100f309c040e3
Petra <ppoklonova@gmail.com>
2015-05-22 13:27:57 +0200

    l

---
+++ public_html/header.php
@@ -0,0 +1,17 @@
+<?php ob_start(); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+                    
+  <head>  
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <script type="text/javascript" src="javascript.js"></script>
+  <link rel="stylesheet" type="text/css" href="default.css">
+  
+  <title></title>
+  </head>
+  <body>
+  
+      
+    <div id="logo">
+        <a href="index.php" class="logo_home"></a>  
+    </div>
